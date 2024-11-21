2c416119ea5f92c0f88680183680fc59a79da094
rzop <139853219+rzop@users.noreply.github.com>
2024-11-20 19:04:36 -0500

    unit testing added

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,31 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen,fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterhead = screen.getByText(/Counter/i); //heavily inspired App.test.js
+  expect(counterhead).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const dispcount = screen.getByText('0');
+  expect(dispcount).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', async () => {
+  const increm = screen.getByRole('button', { name: '+' }); //heavily referenced an example of the React Testing Library link provided
+  fireEvent.click(increm);
+  const neocount = await screen.findByText('1'); 
+  expect(neocount).toBeInTheDocument();
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking - decrements the count', async () => {
+  const decre = screen.getByRole('button', { name: '-' }); //heavily referenced an example of the React Testing Library link provided
+  fireEvent.click(decre);
+  const neocount2 = await screen.findByText('-1');
+  expect(neocount2).toBeInTheDocument();
 });
+
